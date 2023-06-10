6461020a15dc5a30cca5632713f78259005a4e9c
Arnau Aregall <arnau.are@gmail.com>
2023-06-10 12:25:15 +0200

    Adds Spring Cloud Function and Testcontainers (for mockserver) dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,16 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.cloud:spring-cloud-function-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springframework.cloud:spring-cloud-function-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.testcontainers:mockserver:${property("testcontainersVersion")}")
+	testImplementation("org.mock-server:mockserver-client-java:${property("mockServerVersion")}")
 }
 
 dependencyManagement {

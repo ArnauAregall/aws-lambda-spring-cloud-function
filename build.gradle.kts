4c522eecdfe76661f59edf13744dd4c4f13a9fcd
Arnau Aregall <arnau.are@gmail.com>
2023-06-17 23:34:57 +0200

    Remove AwsLambdaHandler and replace aws-serverless-java-container-springboot3 with spring-cloud-function-adapter-aws

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.cloud:spring-cloud-function-context")
 	implementation("org.springframework.cloud:spring-cloud-function-web")
+	implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("com.amazonaws.serverless:aws-serverless-java-container-springboot3:${property("awsServerlessContainerVersion")}")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
